189f416cece1fcf4e236cf51934f8f6421d62542
lbc21street <21street@gmail.com>
2025-11-24 14:15:02 +0300

    Delete HW_04_kthread/um/!build-2.11.sh

--- HW_04_kthread/um/!build-2.11.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#gcc -DUSERMODE -DUBUILD_MODNAME=\"pthread\" ./trace.c ./utils.c ./pthread.c -o pthread -I../inc
-gcc -DUSERMODE -DUBUILD_MODNAME=\"pthread\" ./trace.c ./utils.c ./pthread.c -o pthread_2.11 -I../inc -lpthread -L/usr/toolchain/x86_64-pc-linux-gnu_gcc7.3.0_glibc2.11/x86_64-pc-linux-gnu/lib #-Wl,--dynamic-linker=/usr/toolchain/x86_64-pc-linux-gnu_gcc7.3.0_glibc2.11/x86_64-pc-linux-gnu/lib/ld-linux-x86-64.so.2
